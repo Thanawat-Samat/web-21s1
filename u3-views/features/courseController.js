c90b1bfacd0f85ded1d9d18973f640ecb2fedf67
Thanawat-Samat <thanawatsamat202@gmail.com>
2021-07-09 00:50:15 +0700

    [U3] Navigation breadcrumb include

--- u3-views/features/courseController.js
+++ u3-views/features/courseController.js
@@ -6,7 +6,13 @@ const courseDetails = (req, res) => {
 
   // left 'course' name in handlebars
   // right 'course' js variable
-  res.render('course-details', { course: course })
+  res.render('course-details', {
+    course: course,
+    crumbs: [
+      { href: `/courses/${courseCode}`, text: courseCode }
+    ]
+
+  })
 }
 
 const courseUnits = (req, res) => {
@@ -15,13 +21,27 @@ const courseUnits = (req, res) => {
 
   // left 'course' name in handlebars
   // right 'course.units' js expression
-  res.render('course-units', { units: course.units })
+  res.render('course-units', {
+    course: course,
+    units: course.units,
+    crumbs: [
+      { href: `/courses/${courseCode}`, text: courseCode },
+      { href: `/courses/${courseCode}/units`, text: 'Units' }
+    ]
+
+  })
 }
 
 const courseStudents = (req, res) => {
   const courseCode = req.params.code
   const course = getCourse(courseCode)
-  res.render('course-students', { course: course })
+  res.render('course-students', {
+    course: course,
+    crumbs: [
+      { href: `/courses/${courseCode}`, text: courseCode },
+      { href: `/courses/${courseCode}/students`, text: 'Students' }
+    ]
+  })
 }
 
 module.exports = { courseDetails, courseUnits, courseStudents }
