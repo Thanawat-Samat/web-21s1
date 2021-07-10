1f8646a405572764a4b4abf520a4985a45761470
Thanawat-Samat <thanawatsamat202@gmail.com>
2021-07-10 07:32:14 +0700

    [U3L] Views - DBTube Lab

---
+++ u3-views/features/videoController.js
@@ -0,0 +1,24 @@
+const { getVideo, getSuggestions } = require('../_services/fakeapi')
+
+const videoplayer = (req, res) => {
+  const videoID = req.params.id
+  const video = getVideo(videoID)
+  const teases = getSuggestions(6)
+
+  res.render('video-player', {
+    video: video, teases: teases
+  })
+}
+
+const videoHome = (req, res) => {
+  const cards = getSuggestions(30)
+
+  res.render('video-home', {
+    cards: cards
+  })
+}
+
+module.exports = {
+  videoplayer,
+  videoHome
+}
