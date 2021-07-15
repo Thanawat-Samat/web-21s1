b1608d5c8d9619d5637c2994f8619d0578650021
Thanawat-Samat <thanawatsamat202@gmail.com>
2021-07-15 20:52:39 +0700

    [U4] Book details page

--- u4-databases/app.js
+++ u4-databases/app.js
@@ -1,24 +1,27 @@
 const express = require('express')
 const expHbs = require('express-handlebars')
 const { json, urlencoded } = require('body-parser')
+const { bookDetails } = require('./features/book-controller')
 
 const app = express()
 
-// Tamplates
+// Templates
 app.set('views', './views')
 app.set('view engine', 'hbs')
 app.engine('hbs', expHbs({
   extname: '.hbs',
   defaultLayout: false,
-  partialsDir: ['/views/partials', './views/layouts']
+  partialsDir: ['./views/partials', './views/layouts']
 }))
 
-// middleware
+// Middleware
 app.use(express.static('public'))
 app.use(json())
-app.use(urlencoded({ extname: false }))
+app.use(urlencoded({ extended: false }))
 
 // Routes
+app.get('/')
+app.get('/books/:isbn13', bookDetails)
 // TODO later
 
 const PORT = 3000
