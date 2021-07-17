2065d9aa48c8663654b79f4072a1d45c80a6f09e
Thanawat-Samat <thanawatsamat202@gmail.com>
2021-07-17 23:01:44 +0700

    [U4L] Lab-databases

--- u4-databases/app.js
+++ u4-databases/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const expHbs = require('express-handlebars')
 const { json, urlencoded } = require('body-parser')
 const { bookDetails, bookSearch, bookCreateForm, bookCreate } = require('./features/book-controller')
+const { iterationList, iterationsDetails, studentDetails, courseLists, courseDetails } = require('./features/uni-controller')
 
 const app = express()
 
@@ -24,6 +25,11 @@ app.get('/books', bookSearch)
 app.post('/books', bookCreate)
 app.get('/books/new', bookCreateForm)
 app.get('/books/:isbn13', bookDetails)
+app.get('/iterations', iterationList)
+app.get('/iterations/:code', iterationsDetails)
+app.get('/students/:code', studentDetails)
+app.get('/courses', courseLists)
+app.get('/courses/:code', courseDetails)
 
 app.get('/images/:catchall',
   (_req, res) => res.redirect('/images/404.jpg'))
