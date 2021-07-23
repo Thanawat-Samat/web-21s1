8cd2d28de4b59ccea4228a32778898d93b6cc272
Thanawat-Samat <thanawatsamat202@gmail.com>
2021-07-23 20:20:35 +0700

    [U5] Read one book API

--- u5-rest-apis/app.js
+++ u5-rest-apis/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const { json, urlencoded } = require('body-parser')
-const { readBooks } = require('./features/book-controller')
+const { readBooks, readBook } = require('./features/book-controller')
 
 const app = express()
 
@@ -11,6 +11,7 @@ app.use(urlencoded({ extended: false }))
 
 // Routes
 app.get('/books', readBooks)
+app.get('/books/:isbn13', readBook)
 // TODO later
 
 module.exports = { app }
