0c6c337ca41d37bd4972d40f70091b3263b4526a
Thanawat-Samat <thanawatsamat202@gmail.com>
2021-07-23 20:45:31 +0700

    [U5] Update book API

--- u5-rest-apis/app.js
+++ u5-rest-apis/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const { json, urlencoded } = require('body-parser')
-const { readBooks, readBook, createBook, replaceBook } = require('./features/book-controller')
+const { readBooks, readBook, createBook, replaceBook, updateBook } = require('./features/book-controller')
 
 const app = express()
 
@@ -14,6 +14,7 @@ app.get('/books', readBooks)
 app.get('/books/:isbn13', readBook)
 app.post('/books', createBook)
 app.put('/books/:isbn13', replaceBook)
+app.patch('/books/:isbn13', updateBook)
 // TODO later
 
 module.exports = { app }
