cfd04c530957baa72b33dbcab279b59150ef4b62
Thanawat-Samat <thanawatsamat202@gmail.com>
2021-07-23 20:47:49 +0700

    [U5] Delete book API

--- u5-rest-apis/app.js
+++ u5-rest-apis/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const { json, urlencoded } = require('body-parser')
-const { readBooks, readBook, createBook, replaceBook, updateBook } = require('./features/book-controller')
+const { readBooks, readBook, createBook, replaceBook, updateBook, deleteBook } = require('./features/book-controller')
 
 const app = express()
 
@@ -15,6 +15,7 @@ app.get('/books/:isbn13', readBook)
 app.post('/books', createBook)
 app.put('/books/:isbn13', replaceBook)
 app.patch('/books/:isbn13', updateBook)
+app.delete('/books/:isbn13', deleteBook)
 // TODO later
 
 module.exports = { app }
