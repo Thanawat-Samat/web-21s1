d235127c9aa36730984bcfaa1a5c163d815d0d7e
Thanawat-Samat <thanawatsamat202@gmail.com>
2021-08-21 22:58:59 +0700

    [U6] Routes ,MVVM ,Binding ,V-for ,@Click

--- u6-spas/src/router/index.ts
+++ u6-spas/src/router/index.ts
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '@/views/Home.vue'
+import Colors from '@/views/Colors.vue'
 
 Vue.use(VueRouter)
-
 const routes: Array<RouteConfig> = [
   {
     path: '/',
@@ -11,12 +11,9 @@ const routes: Array<RouteConfig> = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/colors',
+    name: 'Colors',
+    component: Colors
   }
 ]
 
