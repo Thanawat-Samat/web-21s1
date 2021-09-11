c0956da7ce05c65158d8bf16a5de6c7cb7d5c554
Thanawat-Samat <thanawatsamat202@gmail.com>
2021-09-11 22:04:00 +0700

    Create firebase-config.ts

---
+++ u8-state/src/_services/firebase-initialized/firebase-config.ts
@@ -0,0 +1,9 @@
+export default {
+  apiKey: 'AIzaSyA5kY2mHSPEPnAd-WIF_uLzckDKspeGj_s',
+  authDomain: 's1-u8-state-28efc.firebaseapp.com',
+  projectId: 's1-u8-state-28efc',
+  storageBucket: 's1-u8-state-28efc.appspot.com',
+  messagingSenderId: '345981890834',
+  appId: '1:345981890834:web:03413159701b247f10c3cb',
+  measurementId: 'G-3CVYY5EHWJ'
+}
