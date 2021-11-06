81773de818a48392146866f26fc3a248c2ada189
Thanawat-Samat <thanawatsamat202@gmail.com>
2021-11-06 12:56:34 +0700

    Cinema Details

--- proj2-clientside-cinema/hosting/src/router/index.ts
+++ proj2-clientside-cinema/hosting/src/router/index.ts
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 import Home from '../views/Home.vue'
 import CinemaList from '../views/CinemaList.vue'
+import CinemaDetails from '../views/CinemaDetails.vue'
 
 Vue.use(VueRouter)
 
@@ -17,12 +18,9 @@ const routes: Array<RouteConfig> = [
     component: CinemaList
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/cinemas/:slug',
+    name: 'CinemaDetails',
+    component: CinemaDetails
   }
 ]
 
